08c30b2a5d1cfd5cf27204829f9e256b36bb8f36
Alex Lipovka <alex.lipovka@gmail.com>
2026-04-13 23:19:49 +0400

    redefine exports

---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export * from './QuestionnaireResponseForm';
+export * from './QuestionnaireResponseForm/questionnaire-response-form-data';
+export * from './QuestionnaireResponseForm/BaseQuestionnaireResponseForm';
