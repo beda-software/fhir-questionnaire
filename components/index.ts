3f10aeb634fe4d9ca2eb5afd77f802e6b960d9c1
Alex Lipovka <alex.lipovka@gmail.com>
2026-04-20 16:17:08 +0400

    add remote to peerDependences

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export * from './QuestionnaireResponseForm';
 export * from './QuestionnaireResponseForm/questionnaire-response-form-data';
 export * from './QuestionnaireResponseForm/BaseQuestionnaireResponseForm';
+export * from './QuestionnaireResponseForm/BaseQuestionnaireResponseForm/utils';
