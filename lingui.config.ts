e11690cfb09f8b2490c31566700cc572c56d5679
Alex Lipovka <alex.lipovka@gmail.com>
2026-05-01 12:39:29 +0400

    add locales es,de,ru

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,7 +1,7 @@
 import type { LinguiConfig } from '@lingui/conf';
 
 const config: LinguiConfig = {
-    locales: ['en'],
+    locales: ['en', 'ru', 'de', 'es'],
     format: 'po',
     catalogs: [
         {
