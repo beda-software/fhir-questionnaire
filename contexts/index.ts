543342678c69918cb41bb97b59b17803317c4782
Alex Lipovka <alex.lipovka@gmail.com>
2026-05-06 14:36:08 +0400

    revert introduction of itemControlContexts

--- contexts/index.ts
+++ contexts/index.ts
@@ -1,13 +1,7 @@
 import { BaseQuestionnaireResponseFormProps } from '../components';
 import { DebouncedFunc } from 'lodash';
 import { createContext } from 'react';
-import { FormItems, ItemControlGroupItemComponentMapping, ItemControlQuestionItemComponentMapping } from 'sdc-qrf';
-
-export const ItemControlQuestionItemWidgetsContext = createContext<ItemControlQuestionItemComponentMapping>({});
-export const ItemControlGroupItemWidgetsContext = createContext<ItemControlGroupItemComponentMapping>({});
-
-export const ItemControlQuestionItemReadonlyWidgetsContext = createContext<ItemControlQuestionItemComponentMapping>({});
-export const ItemControlGroupItemReadonlyWidgetsContext = createContext<ItemControlGroupItemComponentMapping>({});
+import { FormItems } from 'sdc-qrf';
 
 export * from './valueset-expand';
 
