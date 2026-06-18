e722c217738d80a86ae78230d41da25cd8a41fca
Alex Lipovka <alex.lipovka@gmail.com>
2026-06-18 19:51:10 +0400

    export appendLaunchContextParamenetrs util

--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ export { getFieldErrorMessage } from './components/QuestionnaireResponseForm/Bas
 export * from './contexts/valueset-expand';
 export { ClinicalContext, useClinicalContext, getParameter, getParameterAsFirst } from './contexts/clinical-context';
 export type { ClinicalContextValue } from './contexts/clinical-context';
+export { appendLaunchContextParameters } from './utils';
 export * from './services/valueset-expand';
